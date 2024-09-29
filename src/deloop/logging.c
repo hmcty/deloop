3e93a8943b178f5d104a53489cff27b10258dc76
Harry McCarty <hmccarty@pm.me>
2024-09-29 00:19:58 -0700

    logging update

--- src/deloop/logging.c
+++ src/deloop/logging.c
@@ -1,38 +1,15 @@
 
 #include <stdarg.h>
 #include <stdio.h>
-#include <string.h>
-#include <time.h>
 
 #include "deloop/logging.h"
 
-void deloop_log_format(const char *tag, const char *message, va_list args) {
-  time_t now;
-  time(&now);
-  char *date = ctime(&now);
-  date[strlen(date) - 1] = '\0';
-  printf("%s [%s] ", date, tag);
+void deloop_log_format(const char *file, const int lineno, const char *tag,
+                       const char *message, ...) {
+  va_list args;
+  va_start(args, message);
+  printf("[%s](%s:%d) ", tag, file, lineno);
   vprintf(message, args);
   printf("\n");
-}
-
-void deloop_log_error(const char *message, ...) {
-  va_list args;
-  va_start(args, message);
-  deloop_log_format("error", message, args);
-  va_end(args);
-}
-
-void deloop_log_info(const char *message, ...) {
-  va_list args;
-  va_start(args, message);
-  deloop_log_format("info", message, args);
-  va_end(args);
-}
-
-void deloop_log_debug(const char *message, ...) {
-  va_list args;
-  va_start(args, message);
-  deloop_log_format("debug", message, args);
   va_end(args);
 }
